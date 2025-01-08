2cb1370b7945f4ea1c0ebe11da4701bf17534940
Asad <asadmhmhm@gmail.com>
2025-01-07 20:31:57 -0500

    Language performance testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 // Main function
 
+use std::{thread::sleep, time::{Duration, SystemTime, UNIX_EPOCH}};
+
 fn main() {
-    println!("Hello World!");
+    let start = SystemTime::now();
+    let time_since_epoch= start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    println!("{:?}", time_since_epoch);
+    let start = SystemTime::now();
+    let time_since_epoch= start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    sleep(Duration::from_secs(1));
+    println!("{:?}", time_since_epoch);
+    let start = SystemTime::now();
+    let time_since_epoch= start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    println!("{:?}", time_since_epoch);
+    let start = SystemTime::now();
+    let time_since_epoch= start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    println!("{:?}", time_since_epoch);
+    let start = SystemTime::now();
+    let time_since_epoch= start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    println!("{:?}", time_since_epoch);
 }
